3458223893d0545c1f8213a4c0f2b6e4d4a82129
Robert Ng <robert.ng@xandr.com>
2021-02-02 23:40:58 -0500

    Update main

--- test_main.py
+++ test_main.py
@@ -28,13 +28,13 @@ def test_health(client):
     assert response.json == 'Healthy'
 
 
-# def test_auth(client):
-#     body = {'email': EMAIL,
-#             'password': PASSWORD}
-#     response = client.post('/auth',
-#                            data=json.dumps(body),
-#                            content_type='application/json')
-#
-#     assert response.status_code == 200
-#     token = response.json['token']
-#     assert token is not None
+def test_auth(client):
+    body = {'email': EMAIL,
+            'password': PASSWORD}
+    response = client.post('/auth',
+                           data=json.dumps(body),
+                           content_type='application/json')
+
+    assert response.status_code == 200
+    token = response.json['token']
+    assert token is not None
